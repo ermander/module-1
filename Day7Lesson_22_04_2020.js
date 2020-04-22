7341e5a1587d572b954d24c5b2c53b90b3e74e5d
ermander <61063909+ermander@users.noreply.github.com>
2020-04-22 17:07:27 +0200

    22_04_2020

---
+++ Day7Lesson_22_04_2020.js
@@ -0,0 +1,29 @@
+/* SWITCH CASE */
+
+/* Il programma comincerà ad eseguire la parte di codice ("case") corrispondente. Se nessuna 
+   delle condizioni all'interno dei vari "case" viene ritrovata da codice, verrà eseguito "default"
+   a priori. Per evitare che lo switch case continui ad eseguire gli altri case, è necessario inserire
+   la dicitura "break". Lo switch case viene utilizzato quanto bisogna controllare molte condizioni, 
+   ed è preferibile ad un "if else" annidato molte volte*/  
+
+
+switch(x){
+    case "value1": 
+    // Code
+    // break
+    case "value2": 
+    // Code
+    // break
+    case "value3": 
+    // Code
+    // break
+    case "value4": 
+    // Code
+    // break
+    case "value5": 
+    // Code
+    // break
+    default:
+    // Code
+    // break    
+}
